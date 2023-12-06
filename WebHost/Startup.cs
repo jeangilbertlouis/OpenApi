5b202b70d5e13d46d84553caefa0f73a9619e599
Jean-Gilbert Louis <jeangilbertlouis@gmail.com>
2023-12-06 15:56:05 +0100

    Serialization settings

--- WebHost/Startup.cs
+++ WebHost/Startup.cs
@@ -15,8 +15,7 @@ public class Startup
     }
 
     public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
+    
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllers().AddJsonOptions(opts =>
@@ -25,8 +24,7 @@ public class Startup
         });
         services.AddSingleton<PersonService>();
     }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
         if (env.IsDevelopment())
