a4236efa2d8547eb14786ac5d13dbecb94b4f1b0
Jean-Gilbert Louis <jeangilbertlouis@gmail.com>
2023-12-07 15:31:58 +0100

    Move around some stuff

---
+++ WebHost/Models.cs
@@ -0,0 +1,43 @@
+﻿using NodaTime;
+
+namespace WebHost;
+
+public abstract class Person
+{
+    protected Person(string id, string firstName, string lastName)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public string Id { get; }
+    public string FirstName { get; }
+    public string LastName { get; }
+    public abstract string Type { get; }
+}
+
+public class EmployedPerson : Person
+{
+    public EmployedPerson(string id, string firstName, string lastName, string jobTitle) : base(id, firstName, lastName)
+    {
+        JobTitle = jobTitle;
+    }
+
+    public override string Type => "Employed";
+    public string JobTitle { get; }
+}
+
+public class UnemployedPerson : Person
+{
+    public UnemployedPerson(string id, string firstName, string lastName, string reason, Instant since) : base(id,
+        firstName, lastName)
+    {
+        Reason = reason;
+        Since = since;
+    }
+
+    public override string Type => "Unemployed";
+    public string Reason { get; }
+    public Instant Since { get; }
+}
--- WebHost/PersonsController.cs
+++ WebHost/PersonsController.cs
@@ -26,46 +26,6 @@ public class PersonsController : ControllerBase
     }
 }
 
-public abstract class Person
-{
-    protected Person(string id, string firstName, string lastName)
-    {
-        Id = id;
-        FirstName = firstName;
-        LastName = lastName;
-    }
-
-    public string Id { get; }
-    public string FirstName { get; }
-    public string LastName { get; }
-    public abstract string Type { get; }
-}
-
-public class EmployedPerson : Person
-{
-    public EmployedPerson(string id, string firstName, string lastName, string jobTitle) : base(id, firstName, lastName)
-    {
-        JobTitle = jobTitle;
-    }
-
-    public override string Type => "Employed";
-    public string JobTitle { get; }
-}
-
-public class UnemployedPerson : Person
-{
-    public UnemployedPerson(string id, string firstName, string lastName, string reason, Instant since) : base(id,
-        firstName, lastName)
-    {
-        Reason = reason;
-        Since = since;
-    }
-
-    public override string Type => "Unemployed";
-    public string Reason { get; }
-    public Instant Since { get; }
-}
-
 public class PersonService
 {
     private static readonly List<Person> Db = new()
