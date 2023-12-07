a1f98353049791b51556e6fd159c778fc3524f74
Jean-Gilbert Louis <jeangilbertlouis@gmail.com>
2023-12-07 13:40:28 +0100

    Fix up contract test

--- contracttests/__tests__/people.js
+++ contracttests/__tests__/people.js
@@ -5,26 +5,24 @@ const chaiResponseValidator = require('chai-openapi-response-validator');
 
 describe(`Ensure Open API Spec is satisfied`, function () {
   beforeAll(async () => {
-    chai.use(chaiResponseValidator('C:/Dev3/OpenApi/WebHost/wwwroot/openapi.json'));
+    
+    
+    
+    
+    chai.use(chaiResponseValidator('C:/Dev/OpenApi/WebHost/wwwroot/openapi.json'));
     //OR
     //spec = await axios.get(`http://localhost:11765/openapi.json`);
     //chai.use(chaiResponseValidator(spec.data));
   });
 
-  it('People Endpoint', async function () {
-    const result = await axios.get(`http://localhost:11765/people`)
-    expect(result.status).to.equal(200);
-    expect(result).to.satisfyApiSpec;
-  });
-
   it('Person Jean Endpoint', async function () {
-    const result = await axios.get(`http://localhost:11765/people/1`)
+    const result = await axios.get(`http://localhost:5000/people/1`)
     expect(result.status).to.equal(200);
     expect(result).to.satisfyApiSpec;
   });
 
   it('Person Andreas Endpoint', async function () {
-    const result = await axios.get(`http://localhost:11765/people/2`)
+    const result = await axios.get(`http://localhost:5000/people/2`)
     expect(result.status).to.equal(200);
     expect(result).to.satisfyApiSpec;
   });
